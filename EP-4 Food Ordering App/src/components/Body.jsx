721a52dc3051047cbcee0338cb5172950541265b
Syed Izhaan S <izhaan9802@gmail.com>
2024-02-13 01:32:30 +0530

    useState useRef filter() toggle

--- EP-4 Food Ordering App/src/components/Body.jsx
+++ EP-4 Food Ordering App/src/components/Body.jsx
@@ -1,12 +1,30 @@
 import RestaurantCard from "./RestaurantCard";
 import cardInfo from "../utils/mockData";
+import { useState } from 'react';
+import { useRef } from "react";
+
+// let toggle = {current:true}
+
 const Body = ()=>{
+    const [cardsData,setCardsData] = useState(cardInfo[4].card.card.gridElements.infoWithStyle.restaurants)
+    const toggle = useRef(true);
     return (
         <div className="body">
             <div className="search">search</div>
+            <button onClick={()=>{
+              if(toggle.current===true){
+                toggle.current = false;
+                const filteredCardData = cardsData.filter((restaurant) =>{return restaurant.info.avgRating>=4.5})
+                setCardsData(filteredCardData);
+              }
+              else{
+                toggle.current = true;
+                setCardsData(cardInfo[4].card.card.gridElements.infoWithStyle.restaurants);
+              }
+            }}>Ratings 4.5+</button>
             <div className="restaurant-container">
                 {
-                  cardInfo[4].card.card.gridElements.infoWithStyle.restaurants.map((val)=>{
+                  cardsData.map((val)=>{
                     return <RestaurantCard key={val.info.id} cardData={val}></RestaurantCard>
                   })
                 }
