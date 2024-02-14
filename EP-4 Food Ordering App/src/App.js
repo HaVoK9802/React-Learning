ddc1803278d03dd36e130868934ae6344dcea73d
Syed Izhaan S <izhaan9802@gmail.com>
2024-02-15 03:07:26 +0530

    links client side routing

--- EP-4 Food Ordering App/src/App.js
+++ EP-4 Food Ordering App/src/App.js
@@ -1,16 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {createBrowserRouter,RouterProvider} from "react-router-dom"
+import {createBrowserRouter,RouterProvider,Outlet} from "react-router-dom"
 import Header from "./components/Header";
 import Body from "./components/Body";
-import About from "./components/About"
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Cart from "./components/Cart";
+import Error from "./components/Error";
+
+
 
 
 const AppLayout = ()=>{
     return (
     <div className="main-layout">
         <Header/>
-        <Body/>
+        <Outlet/>
         //FOOTER
     </div>
     );
@@ -19,12 +24,32 @@ const AppLayout = ()=>{
 const router = createBrowserRouter([
     {
         path:'/',
-        element:(<AppLayout></AppLayout>)
-    },
-    {
-        path:'about',
-        element:(<About></About>)
+        element:(<AppLayout></AppLayout>),
+        errorElement:<Error/>,
+        children:[  {
+            
+            path:'/',
+            element:(<Body></Body>),
+            errorElement:<Error/>
+
+        }, 
+        {
+            path:'/about',
+            element:(<About></About>),
+            errorElement:<Error/>
+        },
+        {
+            path:'/contact',
+            element:(<Contact></Contact>),
+            errorElement:<Error/>
+        },
+        {
+            path:'/cart',
+            element:(<Cart></Cart>),
+            errorElement:<Error/>
+        }]
     }
+
 ])
 
 
